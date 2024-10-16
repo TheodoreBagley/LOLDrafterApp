72e95f5ff21ba12c99d68ef883fbfe2b7e7952c9
Alan-Zetzer <ajzetzer@gmail.com>
2024-10-16 11:15:49 -0600

    champ object to scrape lane specific data from counterstrats

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
+
+class Champion:
+    def __init__(self, name: str, lanes: list):
+        self.name = name
+        self.lanes = lanes
+
+    def __repr__(self):
+        return f"Champion(name='{self.name}', lanes={self.lanes})"
+
+
 champs_list = []
 
 url = 'https://www.counterstats.net/'
@@ -9,23 +21,27 @@ response = requests.get(url)
 if response.status_code == 200:
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    #print(soup.prettify()[:100000])
+
 
     champion_divs = soup.find_all('div', class_='champion-icon champList')
 
     #champs_list = []
 
     for div in champion_divs:
-        champion_id = div.get('id')
-        if champion_id:
+        champion_name = div.get('id')
+        if champion_name:
             #print(champion_id)
-            if '-' in champion_id:
-                champion_id = champion_id.replace('-', '')
+            if '-' in champion_name:
+                champion_id = champion_name.replace('-', '')
                 print(champion_id)
-            if "nunu" in champion_id:
+            if "nunu" in champion_name:
                 champion_id = "nunu"
-            champs_list.append(champion_id.strip())
-
+            #champs_list.append(champion_id.strip())
+        champion_lanes = div.get('data-lanes')
+        lanes = re.findall(r'jungle|top|mid|adc|support', champion_lanes)
+        champs_list.append(Champion(champion_name, lanes))
+    for champ in champs_list:
+        print(champ)
     #print(champs_list)
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
@@ -33,7 +49,7 @@ else:
 
 
 
-# TODO: https://lolalytics.com/lol/<champ i>/vs/<champ j>/build/?tier=master_plus
+# TODO: https://www.counterstats.net/league-of-legends/
 
 
 for champ in champs_list:
@@ -43,4 +59,5 @@ for champ in champs_list:
         data = response = requests.get(url)
         soupy = BeautifulSoup(data.content, 'html.parser')
         para = soupy.find('div', attrs={'q:key': 'yJ_1'})
-        print(para)      
+        #print(para)
+
